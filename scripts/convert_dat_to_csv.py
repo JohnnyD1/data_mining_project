56a325c53e6ed85e922677b299fc1bad8992aa2d
JohnnyD1 <johndeme@buffalo.edu>
2019-11-21 12:39:02 -0500

    convert data from dat to csv

---
+++ scripts/convert_dat_to_csv.py
@@ -0,0 +1,22 @@
+import sys
+import csv
+
+
+def convert(dat_file):
+    with open(dat_file, 'r') as dat_f, open(''.join(dat_file.rsplit('.',1)[0]) + '.csv', 'w') as csv_f:
+        csv_writer = csv.writer(csv_f)
+
+        for line in dat_f:
+            row = line.split(' ')
+            csv_writer.writerow(row)
+
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        convert(sys.argv[1])
+    else:
+        print("need to supply argument")
+        exit()
